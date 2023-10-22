b508953446c5c397a048f504f3fc49e2519bc687
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-22 17:14:19 +0300

    Add class Constants

---
+++ src/main/java/com/example/app/SpringBootWebPhop/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.example.app.SpringBootWebPhop.utils;
+
+public class Constants {
+    public final static String BASE_URL = "http://localhost:8080/";
+    public final static String URL_IMAGES_UPLOADS = "images/uploads/";
+    public final static String URL_FILE_UPLOADS = "./images/uploads/";
+    public final static String INPUT_REQ_MSG = "Input required.";
+    public final static String ADD_FILE_MSG = "Add file.";
+    public final static String SAVED_MSG = "Saved.";
+    public final static String ORDER_DEFAULT_STATUS = "processing";
+}
