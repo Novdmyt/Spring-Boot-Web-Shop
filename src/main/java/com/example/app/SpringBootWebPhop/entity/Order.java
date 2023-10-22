b0affcf5184922c408a863f20cbd3cfce5a999c1
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-22 17:49:44 +0300

    Add @NoArgsConstructor

--- src/main/java/com/example/app/SpringBootWebPhop/entity/Order.java
+++ src/main/java/com/example/app/SpringBootWebPhop/entity/Order.java
@@ -3,9 +3,11 @@ package com.example.app.SpringBootWebPhop.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "orders")
 public class Order {
